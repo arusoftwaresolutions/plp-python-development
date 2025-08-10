027fd91b9616c8f964fb0493da1e5440b843f626
Araya Haftu <arayahaftu1229@gmail.com>
2025-08-10 13:20:05 +0300

    Create python_week2_assignment.py

---
+++ python_week2_assignment.py
@@ -0,0 +1,21 @@
+my_list = []
+"""appending elements to my list"""
+my_list.append(10)
+my_list.append(20)
+my_list.append(30)
+my_list.append(40)
+
+"""inserting an element to mylist"""
+my_list.insert(1,15)
+
+"""extending of my list with another kist"""
+my_list.extend([50,60,70])
+
+"""removing the last element from the mylist """
+my_list.pop()
+
+"""sortingthe list in ascending order"""
+my_list.sort()
+
+"""searching index of a number in alist"""
+print(my_list.index(30))
